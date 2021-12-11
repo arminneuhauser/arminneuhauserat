4bd872b8fca232dc7a6f4fef35cf5a5794078a3d
arminneuhauser <aman@gmx.at>
2021-12-11 21:43:31 +0100

    footer

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -1,20 +1,32 @@
+<script lang="ts">
+    import * as animateScroll from "svelte-scrollto";
+    import up from './up.svg?raw';
+
+    let now = new Date(),
+        year = now.getFullYear();
+</script>
+
 <footer>
     <div>
+        <h1>Lass uns gemeinsam Großes erschaffen</h1>
+        <p>
+            Jede Ausfahrt sollte mit einem guten Kaffee abgeschlossen werden.<br> Also komm vorbei und sag Hallo.
+        </p>
+        <a href="mailto:mail@arminneuhauser.at">mail@arminneuhauser.at</a>
+        <hr>
         <div>
-            <h1>Lass uns gemeinsam Großes machen</h1>
-            <p>
-                Jede Ausfahrt sollte mit einem guten Kaffee abgeschlossen werden.<br> Also komm vorbei und sag Hallo.
-            </p>
-            <a href="mailto:mail@arminneuhauser.at">mail@arminneuhauser.at</a>
+            <small>Armin Neuhauser © {year}</small>
+            <button on:click={() => animateScroll.scrollToTop()} title="Zurück nach oben">
+                {@html up}
+            </button>
         </div>
-        <button>Zurück nach oben</button>
     </div>
 </footer>
 
 <style lang="scss">
     footer {
         background-color: var(--base);
-        padding: #{fn.rem(10)} var(--core-padding);
+        padding: #{fn.rem(70)} var(--core-padding) var(--core-padding);
 
         @media (min-width: var.$breakpoint-md) {
             padding-top: #{fn.rem(30)};
@@ -22,21 +34,20 @@
         }
 
         > div {
-            display: grid;
-            grid-gap: #{fn.rem(16)};
-            align-items: flex-end;
             max-width: var(--core-max-width);
             margin: 0 auto;
 
-            @media (min-width: var.$breakpoint-md) {
-                grid-template-columns: 1fr auto;
+            > div {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
             }
         }
 
         h1 {
             font-family: var(--serif);
             font-weight: 700;
-            font-size: fn.rfs(24, 36);
+            font-size: fn.rfs(32, 64);
             line-height: 1.1;
         }
 
@@ -55,8 +66,20 @@
             }
         }
 
+        small {
+            opacity: 0.5;
+        }
+
         button {
-            text-align: right;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: #{fn.rem(10)};
+            margin-right: #{fn.rem(-10)};
+        }
+
+        hr {
+            margin: #{fn.rem(40)} 0 #{fn.rem(20)};
         }
     }
 </style>
