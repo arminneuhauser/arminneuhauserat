f8a38e326e1431a10b25ae4b59db2907b620f92b
arminneuhauser <aman@gmx.at>
2022-01-08 11:42:39 +0100

    kontakt

--- src/routes/kontakt.svelte
+++ src/routes/kontakt.svelte
@@ -27,28 +27,36 @@
 </h1>
 
 <section>
+    <header>
+        <p>
+            Ich bin immer auf der Suche nach neuen Ideen und spannenden Projekten. Zögere nicht, mir zu schreiben.
+        </p>
+    </header>
     <div>
         <a class="mail" href="mailto:mail@arminneuhauser.at"><span>mail<span class="flickr">@</span>arminneuhauser.at</span></a>
     </div>
 </section>
 
 <section>
-    <div>
+    <header>
+        <h3>Lass uns dein Projekt verwirklichen</h3>
         <p>
-            Ich bin immer auf der Suche nach neuen Ideen und spannenden Projekten.
+            Ich bin immer auf der Suche nach neuen Ideen und spannenden Projekten. Fülle das Kontaktformular aus, wenn du mit mir zusammenarbeiten möchtest.
         </p>
-        <form>
+    </header>
+    <div>
+        <form name="contact" method="POST" data-netlify="true">
             <div>
                 <label for="name">Name</label>
-                <input id="name" type="text" placeholder="Ihr Name">
+                <input id="name" name="name" type="text" placeholder="Dein Name">
             </div>
             <div>
                 <label for="email">E-Mail-Adresse</label>
-                <input id="email" type="email" placeholder="Ihre E-Mail-Adresse">
+                <input id="email" name="email" type="email" placeholder="Deine E-Mail-Adresse">
             </div>
             <div>
                 <label for="message">Nachricht</label>
-                <textarea id="message" placeholder="Ihre Nachricht an mich" rows="5"></textarea>
+                <textarea id="message" name="message" placeholder="Deine Nachricht an mich" rows="5"></textarea>
             </div>
             <button type="submit">
                 Absenden
@@ -60,17 +68,26 @@
 
 <style lang="scss">
     @use "src/scss/forms.scss";
+    @use "src/scss/animations.scss";
 
     section {
         box-sizing: border-box;
         padding: var(--core-padding);
 
         > div {
-            display: grid;
-            grid-gap: var(--core-padding);
-            justify-content: center;
             margin: 0 auto;
-            max-width: var(--core-max-width);
+            max-width: var(--content-max-width);
+        }
+
+        > header {
+            margin: 0 auto #{fn.rem(60)};
+            max-width: var(--content-max-width);
+            border-left: 1px solid var(--on-base);
+            padding-left: #{fn.rem(15)};
+        }
+
+        &:last-child {
+            padding-bottom: calc(var(--core-padding) * 3);
         }
     }
 
@@ -82,6 +99,10 @@
         width: 100vw;
         margin: #{fn.rem(120)} 0 #{fn.rem(70)};
         text-transform: uppercase;
+
+        @media (min-width: var.$breakpoint-lg) {
+            margin-top: #{fn.rem(180)};
+        }
 
         span {
             display: inline-block;
@@ -101,6 +122,15 @@
         }
     }
 
+    h3 {
+        font-size: #{fn.rfs(24, 34)};
+    }
+
+    p {
+        font-size: #{fn.rfs(16, 18)};
+        max-width: #{fn.rem(600)};
+    }
+
     form {
         > div {
             margin-bottom: #{fn.rem(30)};
@@ -108,39 +138,44 @@
     }
 
     .mail {
-        // text-decoration: underline;
-        font-size: #{fn.rem(24)};
+        font-size: #{fn.rfs(22, 50, 320, 768)};
         font-weight: 500;
         
-        span {
+        > span {
             position: relative;
             z-index: 1;
-            text-shadow: 0 0 0.033em #fff, 0 0 0.08em #fff, 0 0 0.1em var(--primary), 0 0 0.2em var(--primary), 0 0 0.3em var(--primary), 0 0 1em var(--primary), 0 0 1.5em var(--primary);
-        }
-    }
 
-    .flickr {
-        animation: flickr 3s linear infinite alternate forwards;
-    }
+            &::before {
+                content: '';
+                position: absolute;
+                left: 0;
+                width: 100%;
+                bottom: -0.05em;
+                border-bottom: 0.1em solid var(--on-base);
+                transition: transform 0.25s var(--easing);
+                pointer-events: none;
+            }
 
-    @keyframes flickr {
-        0%,
-        19.999%,
-        22%,
-        62.999%,
-        64%,
-        64.999%,
-        72%,
-        100% {
-            opacity: 1;
+            :global([color-scheme="dark"]) & {
+                text-shadow: 0 0 0.033em #fff, 0 0 0.08em #fff, 0 0 0.1em var(--primary), 0 0 0.2em var(--primary), 0 0 0.3em var(--primary), 0 0 1em var(--primary), 0 0 1.5em var(--primary);
+
+                &::before {
+                    box-shadow: 0 0 0.033em #fff, 0 0 0.08em #fff, 0 0 0.1em var(--primary), 0 0 0.2em var(--primary), 0 0 0.3em var(--primary), 0 0 1em var(--primary), 0 0 1.5em var(--primary);
+                }
+
+                .flickr {
+                    animation: flickr 3s linear infinite alternate forwards;
+                }
+            }
+
         }
-        20%,
-        21.999%,
-        63%,
-        63.999%,
-        65%,
-        71.999% {
-            opacity: 0.3;
+
+        &:hover {
+            > span {
+                &::before {
+                    transform: translateY(0.1em);
+                }
+            }
         }
     }
 </style>
